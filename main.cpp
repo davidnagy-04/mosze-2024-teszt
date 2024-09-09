0910b7cbbb017dc2547f8b626f2086557ed5656f
davidnagy-04 <davidnagy20011104@gmail.com>
2024-09-09 12:32:34 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,16 +4,16 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
+    int *b = new int[N_ELEMENTS]; //  _ hiányzik
     std::cout << '1-100 ertekek duplazasa'
     for (int i = 0;)
     {
         b[i] = i * 2;
     }
     
-    for (int i = 0; i; i++)
+    for (int i = 0; i < b.length; i++) // tömb hossz tartó ciklus hiányzása
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:"<< b[i] ;// tömb hiányzik
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
